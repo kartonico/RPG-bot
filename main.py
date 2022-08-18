30d49fcf669e09ad4da47edc3614f90199fcccbb
kartonico <luizmiguelsilva488@gmail.com>
2022-08-18 19:32:25 -0300

    import discord import random
    d8 = random.randrange(8)
    
    class MyCLient(discord.Client):
        async def on_ready(self):
            print(f'Logged on as {self.user}!')
    
        async def on_message(self, message):
            if message.author == client.user:
                return
    
            if message.content.startswith('?hi'):
                await message.channel.send('HI brother! ')
    
            if message.content.startswith('?d8'):
                await message.channel.send(f'@{message.author}: {d8}')
    
    client = MyCLient()
    client.run('MTAwOTYzMzc3Mjk1OTk3MzUwOA.Gm1FOX.fAcJRCLbsUtIqgRX_wPdMAw3KB005Iq97M6XP4')

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import random
+
+#configurando o dado:
+d8 = random.randrange(8)
+
+#codigo base:
+class MyCLient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+    
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('?hi'):
+            await message.channel.send('HI brother! ')
+
+        if message.content.startswith('?d8'):
+            await message.channel.send(f'@{message.author}: {d8}')
+
+#ligando o bot
+client = MyCLient()
+#token do bot
+client.run('MTAwOTYzMzc3Mjk1OTk3MzUwOA.Gm1FOX.fAcJRCLbsUtIqgRX_wPdMAw3KB005Iq97M6XP4')
