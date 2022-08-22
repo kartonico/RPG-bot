1e67dd1896dd4081b20d8d273cf257eb55f040fb
kartonico <luizmiguelsilva488@gmail.com>
2022-08-21 21:57:09 -0300

    update001

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import discord
 import random
+import dice as dc
 
-#codigo base:
+#code base:
 class MyCLient(discord.Client):
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f'{self.user} on action!')
+        print('___/(0-0)\___')
     
     async def on_message(self, message):
         if message.author == client.user:
             return
 
-        if message.content.startswith('-'):
-            await message.channel.send('This command is incomplete or wrong! Use  "-help" to learn more about the commands.')
+#Defining functions:
+        if message.content.startswith('-help'):
+            await message.channel.send('In construction.')
 
-        if message.content.startswith('-d8'):
-            d8 = random.choice([1,2,3,4,5,6,7,8])
-            await message.channel.send(f'{message.author.mention} tirou {d8}')
-#ligando o bot
+        if message.content.startswith('D4'):
+            dice = random.choice(dc.d4)
+            await message.channel.send(f'{message.author.mention} took out: {dice}')
+
+        if message.content.startswith('D6'):
+            dice = random.choice(dc.d6)
+            await message.channel.send(f'{message.author.mention} took out: {dice}')
+
+        if message.content.startswith('D8'):
+            dice = random.choice(dc.d8)
+            await message.channel.send(f'{message.author.mention} took out: {dice}')
+
+        if message.content.startswith('!D10'):
+            dice = random.choice(dc.d10)
+            await message.channel.send(f'{message.author.mention} took out: {dice}')
+
+        if message.content.startswith('D12'):
+            dice = random.choice(dc.d12)
+            await message .channel.send(f'{message.author.mention} took out: {dice}')
+        
+        if message.content.startswith('D20'):
+            dice = random.choice(dc.d20)
+            await message.channel.send(f'{message.author.mention} took out: {dice}')
+
+        if message.content.startswith('-d100'):
+            dice = random.choice(dc.d100)
+            await message.channel.send(f'{message.author.mention} took out: {dice}')
+        
+#starting the bot:
 client = MyCLient()
-#token do bot
 client.run('Your token here.')
